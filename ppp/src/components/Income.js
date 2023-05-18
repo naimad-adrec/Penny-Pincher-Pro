9a37eeecd797810abc7e241fa176f8d2a3c50c42
Nasrullah-Ahmed <nasrullahmed2@gmail.com>
2023-05-17 22:20:47 -0700

    created header

---
+++ ppp/src/components/Income.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './income.css'
+
+const Income = () => {
+  return (
+    <div className='income'>
+        <h1 className='C'>Penny Pincher Pro</h1>
+    </div>
+  )
+}
+
+export default Income
